e82d6f7a5207e6313e84ca2c3bb44a6ecd79bde8
Ivan Solomichev <vanijatko.social@gmail.com>
2018-10-02 12:06:43 +0300

    Improvements made

--- contracts/CarExchange.sol
+++ contracts/CarExchange.sol
@@ -1,13 +1,17 @@
-pragma solidity 0.4.24;
+pragma solidity ^0.4.24;
 
-
+//  TODO: uze OpenZeppelin, implement as Ownable, use SafeMath for uint256
 contract CarExchange {
-  struct RegisteredCars {
+  struct Car {
     address carOwner;
     string vinNumber;
   }
-  mapping (uint => RegisteredCars) registeredCars;
-  uint carNameIndex;
+
+  uint256 public carAmount;
+  //  use explicit uint256
+  mapping (uint256 => Car) public carDetails;  //  we have only registered cars here
+  mapping (address => uint256[]) private ownerCars;  //  owner => carIndexes[]
+  mapping (bytes32 => address) private carOwner;
 
   //EVENTS//
   event Registered(string _vinNumber, address indexed _owner);
@@ -19,33 +23,44 @@ contract CarExchange {
   // start of implementation function register(address _owner, string _vinNumber)
 
   function register(address _owner, string _vinNumber) public returns (bool){
-    require(!hasCar(_vinNumber));
-    require(carNameIndex + 1 > carNameIndex);
-    bytes memory str = bytes(_vinNumber);
-    require(str.length == 17);
-    carNameIndex++;
-    registeredCars[carNameIndex].carOwner = _owner;
-    registeredCars[carNameIndex].vinNumber = _vinNumber;
+    require(_owner != address(0), "can not be 0");
+    require(bytes(_vinNumber).length == 17, "wrong vin length");
+    require(carOwner[convert(_vinNumber)] == address(0), "vin is already registered");
+
+    carAmount += 1; //  TODO: use SafeMath
+    carDetails[carAmount] = Car(_owner, _vinNumber);
+    
+    carOwner[convert(_vinNumber)] = _owner;
+    ownerCars[_owner].push(carAmount);
+    
     emit Registered(_vinNumber, _owner);
   }
+  
+  function ownerForCar(string _vinNumber) public view returns (address) {
+      return carOwner[convert(_vinNumber)];
+  }
+  
+  function carsForOwner(address _owner) public view returns (uint256[]) {
+      return ownerCars[_owner];
+  }
 
-  function hasCar(string vinNumber) internal view returns (bool) {
-        uint8 index = getCarIndex(vinNumber);
-        if (index == 0) {
-            return false;
-        }
-        return true;
-    }
+  // function hasCar(string vinNumber) internal view returns (bool) {
+  //       uint8 index = getCarIndex(vinNumber);
+  //       if (index == 0) {
+  //           return false;
+  //       }
+  //       return true;
+  //   }
 
-    //We must check if the car with such vinNumber already exist
-    function getCarIndex(string vinNumber) internal view returns (uint8) {
-        for (uint8 i = 1; i <= carNameIndex; i++) {
-            if (stringsEqual(registeredCars[i].vinNumber, vinNumber)) {
-                return i;
-            }
-        }
-        return 0;
-    }
+  //   //We must check if the car with such vinNumber already exist
+  //   function getCarIndex(string vinNumber) internal view returns (uint8) {
+  //       for (uint8 i = 1; i <= carNameIndex; i++) {
+  //           if (stringsEqual(registeredCars[i].vinNumber, vinNumber)) {
+  //               return i;
+  //           }
+  //       }
+  //       return 0;
+  //   }
   // end of implementation function register(address _owner, string _vinNumber)
 
   // buy a car by _vinNumber that is listed for sale
@@ -63,22 +78,28 @@ contract CarExchange {
 
   } */
 
-
-
   // STRING COMPARISON FUNCTION //
 
-  function stringsEqual(string storage _a, string memory _b) internal view returns (bool) {
-    bytes storage a = bytes(_a);
-    bytes memory b = bytes(_b);
-      if (a.length != b.length) {
-        return false;
-      }
-      // @todo unroll this loop
-      for (uint i = 0; i < a.length; i ++) {
-        if (a[i] != b[i]) {
-          return false;
-        }
-      }
-      return true;
+//   function stringsEqual(string storage _a, string memory _b) internal view returns (bool) {
+//     bytes storage a = bytes(_a);
+//     bytes memory b = bytes(_b);
+//       if (a.length != b.length) {
+//         return false;
+//       }
+//       // @todo unroll this loop
+//       for (uint i = 0; i < a.length; i ++) {
+//         if (a[i] != b[i]) {
+//           return false;
+//         }
+//       }
+//       return true;
+//   }
+
+  function convert(string key) private pure returns (bytes32 ret) {
+    require(bytes(key).length <= 32);
+
+    assembly {
+      ret := mload(add(key, 32))
+    }
   }
 }
