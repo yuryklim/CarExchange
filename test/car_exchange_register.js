6132930a0ece8dd046a10c857d7688b81eb4834c
yuryklim <yuryklim@outlook.com>
2018-10-11 22:26:05 +0300

    Implementation of expectThrow and making all TODO

--- test/car_exchange_register.js
+++ test/car_exchange_register.js
@@ -1,75 +1,78 @@
+const { expectThrow } = require ('../node_modules/openzeppelin-solidity/test/helpers/expectThrow');
+const { EVMRevert } = require ('../node_modules/openzeppelin-solidity/test/helpers/EVMRevert');
+
+const CarExchange = artifacts.require("./CarExchange.sol");
+
 //  TODO: add empty lines, tabs
-const carExchange = artifacts.require("./CarExchange.sol");
+//  IVAN: empty lines, tabs were added
+
 contract('car_exchange_register', function (accounts) {
+  beforeEach(async () => {
+    carExchange = await CarExchange.new();
+  });
+
   it("should be possible to register a new car", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
-    let registerCar = await sender.register(accountA, "1HGBH41JXMN109186", 5);
-    assert.equal(await sender.ownerForCar("1HGBH41JXMN109186"), accountA, "accountA is not owner");
+    let registerCar = await carExchange.register(accountA, "1HGBH41JXMN109186", 5);
+    assert.equal(await carExchange.ownerForCar("1HGBH41JXMN109186"), accountA, "accountA is not owner");
     assert.equal(registerCar.logs[0].event, "Registered", "Registered event should be emitted");
   });
+
   it("should be possible to register a new car with the same account", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
-    await sender.register(accountA, "1HGBH41JXMN109186", 5);
-    //console.log((await sender.register(accountA, "2HGBH41JXMN109186")).logs[0].event);
-    await sender.register(accountA, "1HGBH41JXMN109187", 5);
-    assert.equal(await sender.ownerForCar("1HGBH41JXMN109187"), accountA, "accountA is not owner");
-    assert.equal(await sender.ownerForCar("1HGBH41JXMN109186"), accountA, "accountA is not owner");
+    await carExchange.register(accountA, "7HGBH41JXMN109186", 5);
+    await carExchange.register(accountA, "8HGBH41JXMN109187", 5);
+    assert.equal(await carExchange.ownerForCar("7HGBH41JXMN109186"), accountA, "accountA is not owner");
+    assert.equal(await carExchange.ownerForCar("8HGBH41JXMN109187"), accountA, "accountA is not owner");
   });
+
   it("should revert if we try to register the car with the vinNumber that already exists", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
-    await sender.register(accountA, "1HGBH41JXMN109188", 5);
-    try {
-      await sender.register(accountA, "1HGBH41JXMN109188", 5);
-    } catch (e) {
-      assert.equal(e.message, "VM Exception while processing transaction: revert vin is already registered", "Error should be throwed");
-    }
+    await carExchange.register(accountA,  "1HGBH41JXMN109188",  5);
+    await expectThrow(
+      carExchange.register(accountC,  "1HGBH41JXMN109188",  5),
+        EVMRevert,
+    );
   });
+
   it("should revert if we try to register the car with the vinNumber that is less then 17 characters", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
     //  TODO:  please, reimplement using expectThrow helper in all places
-    try {
-      await sender.register(accountB, "1HGBH41JXMN", 5);
-    } catch (e) {
-      assert.equal(e.message, "VM Exception while processing transaction: revert wrong vin length", "Error should be throwed");
-    }
+    //  IVAN: reimplement using expectThrow helper in all places was done
+    await expectThrow(
+      carExchange.register(accountC,  "1HGBH41JXMN1091",  5),
+        EVMRevert,
+    );
   });
 
   it("should revert if we try to register the car with the vinNumber that is higher then 17 characters", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
-    try {
-      await sender.register(accountC, "1HGBH41JXMN109188909", 5);
-    } catch (e) {
-      assert.equal(e.message, "VM Exception while processing transaction: revert wrong vin length", "Error should be throwed");
-    }
+    await expectThrow(
+      carExchange.register(accountC,  "1HGBH41JXMN10918899",  5),
+        EVMRevert,
+    );
   });
+
   it("should revert if we try to register the car with the 0x0 account", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
-    try {
-      await sender.register("0x0", "1HGBH41JXMN109188", 5);
-    } catch (e) {
-      assert.equal(e.message, "VM Exception while processing transaction: revert address can not be 0", "Error should be throwed");
-    }
+    await expectThrow(
+      carExchange.register(0x0,  "2HGBH41JXMN109188",  5),
+        EVMRevert,
+    );
   });
+
   it("should revert if we try to register the car with the carPrice 0", async () => {
-    sender = await carExchange.new();
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
-    try {
-      await sender.register(accountD, "1HGBH41JXMN109188", 0);
-    } catch (e) {
-      assert.equal(e.message, "VM Exception while processing transaction: revert price of car can not be 0", "Error should be throwed");
-    }
+    await expectThrow(
+      carExchange.register(accountC,  "3HGBH41JXMN109188",  0),
+        EVMRevert,
+    );
   });
 });
