caea3ca0eaf60ad511e49e7f73abc9fa7b15003f
yuryklim <yuryklim@outlook.com>
2018-10-12 20:08:03 +0300

    Add some improvments

--- test/car_exchange_register.js
+++ test/car_exchange_register.js
@@ -14,7 +14,9 @@ contract('car_exchange_register', function (accounts) {
   it("should be possible to register a new car", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     let registerCar = await carExchange.register(accountA, "1HGBH41JXMN109186", 5);
+
     assert.equal(await carExchange.ownerForCar("1HGBH41JXMN109186"), accountA, "accountA is not owner");
     assert.equal(registerCar.logs[0].event, "Registered", "Registered event should be emitted");
   });
@@ -22,8 +24,10 @@ contract('car_exchange_register', function (accounts) {
   it("should be possible to register a new car with the same account", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     await carExchange.register(accountA, "7HGBH41JXMN109186", 5);
     await carExchange.register(accountA, "8HGBH41JXMN109187", 5);
+
     assert.equal(await carExchange.ownerForCar("7HGBH41JXMN109186"), accountA, "accountA is not owner");
     assert.equal(await carExchange.ownerForCar("8HGBH41JXMN109187"), accountA, "accountA is not owner");
   });
@@ -31,7 +35,9 @@ contract('car_exchange_register', function (accounts) {
   it("should revert if we try to register the car with the vinNumber that already exists", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     await carExchange.register(accountA,  "1HGBH41JXMN109188",  5);
+
     await expectThrow(
       carExchange.register(accountC,  "1HGBH41JXMN109188",  5),
         EVMRevert,
@@ -41,8 +47,10 @@ contract('car_exchange_register', function (accounts) {
   it("should revert if we try to register the car with the vinNumber that is less then 17 characters", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     //  TODO:  please, reimplement using expectThrow helper in all places
     //  IVAN: reimplement using expectThrow helper in all places was done
+
     await expectThrow(
       carExchange.register(accountC,  "1HGBH41JXMN1091",  5),
         EVMRevert,
@@ -52,6 +60,7 @@ contract('car_exchange_register', function (accounts) {
   it("should revert if we try to register the car with the vinNumber that is higher then 17 characters", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     await expectThrow(
       carExchange.register(accountC,  "1HGBH41JXMN10918899",  5),
         EVMRevert,
@@ -61,6 +70,7 @@ contract('car_exchange_register', function (accounts) {
   it("should revert if we try to register the car with the 0x0 account", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     await expectThrow(
       carExchange.register(0x0,  "2HGBH41JXMN109188",  5),
         EVMRevert,
@@ -70,6 +80,7 @@ contract('car_exchange_register', function (accounts) {
   it("should revert if we try to register the car with the carPrice 0", async () => {
     let accountA, accountB, accountC, accountD;
     [accountA, accountB, accountC, accountD] = accounts;
+
     await expectThrow(
       carExchange.register(accountC,  "3HGBH41JXMN109188",  0),
         EVMRevert,
